a103ec92f721b1a128bc6556a857ddf7ff83cb54
w-aaron <w.aaronllan@gmail.com>
2026-07-11 08:39:34 +0800

    Solution for 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const len = arguments.length;
+    if(len > 1){
+        for(let i = 1; i < len; i++){
+            let remove = arguments[i];
+            let included = arr.includes(remove);
+            let index = arr.indexOf(remove);
+            if(included && index >= 0){
+                arr.splice(index, 1);
+                if(included){
+                    i--;
+                }
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
